bb0626fd57c9c786bc06d28dbcd97bbb5a39d967
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-10-01 13:45:34 +0300

    Remove haskell default thing.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -44,8 +44,6 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-(setq haskell-process-type 'haskell-stack-ghc)
-
 (setq doom-font (font-spec :family "Iosevka Fixed Medium" :size 15))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
