c78f618366c54371b07f612e46debab5effce800
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-10-23 13:32:00 +0300

    First steps: Keybindings, ewmh, xmobar..

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,22 +1,30 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.EwmhDesktops
 
 main = do
-  xmonad $ def
-        { terminal    = myTerminal
-        , modMask     = myModMask
-        , borderWidth = myBorderWidth
+  xmonad . ewmhFullscreen . ewmh  $ def
+        { terminal           = myTerminal
+        , modMask            = myModMask
+        , borderWidth        = myBorderWidth
+        , focusedBorderColor = "#749F82"
+        , normalBorderColor  = "#425F57"
         }
       `additionalKeysP`
         [  ("M-e", spawn myEmacs)
-        ,  ("M-b", spawn myBrowser)
+        ,  ("M-v", spawn myBrowser)
         ,  ("<XF86MonBrightnessUp>", spawn "brightnessctl s +2%")
         ,  ("<XF86MonBrightnessDown>", spawn "brightnessctl s 2%-")
+        ,  ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 3%+")
+        ,  ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 3%-")
         ,  ("M-<Return>", spawn myTerminal)
         ]
 
 myTerminal = "alacritty"
 myModMask = mod4Mask
-myBorderWidth = 3
+myBorderWidth = 5
 myEmacs = "emacsclient -c -a 'emacs' "
 myBrowser = "firefox"
