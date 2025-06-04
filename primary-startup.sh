a6513ce82b86a255f44684f308596aaa7cb7cb92
Your Name <you@example.com>
2025-06-04 12:20:10 +0300

    Update the primary-startup script of my main workstation

--- primary-startup.sh
+++ primary-startup.sh
@@ -2,3 +2,7 @@
 
 setxkbmap -option "caps:ctrl_modifier"
 xset r rate 350 30
+xrandr --output DisplayPort-0 --primary --mode 3440x1440 --rate 143.97 --output DisplayPort-1 --mode 2560x1440 --rate 144 --left-of DisplayPort-0
+
+# TODO: Improve this
+redshift -O 4000
